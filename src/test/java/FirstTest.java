06dfeafb4fb69bb9a5cc68a0c29eb7938ad259c8
Vadym Feshchenko <vadym.feshchenko@playtech.com>
2016-08-07 17:18:18 +0300

    Add Selenium 2.53.1 and ThreadLocal<WebDriver> for avoid race-condition

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -4,19 +4,19 @@ public class FirstTest extends TestRunner {
 
     @Test
     public void stubTest01(){
-        long id = Thread.currentThread().getId();
-        System.out.println("Before test-method01. Thread id is: " + id);
+        System.out.println("Before test-method01. Thread id is: " + Thread.currentThread().getId());
+        threadLocalDriver.get().get("http://google.com/");
     }
 
     @Test
     public void stubTest02(){
-        long id = Thread.currentThread().getId();
-        System.out.println("Before test-method02. Thread id is: " + id);
+        System.out.println("Before test-method02. Thread id is: " + Thread.currentThread().getId());
+        threadLocalDriver.get().get("http://yahoo.com/");
     }
 
     @Test
     public void stubTest03(){
-        long id = Thread.currentThread().getId();
-        System.out.println("Before test-method03. Thread id is: " + id);
+        System.out.println("Before test-method03. Thread id is: " + Thread.currentThread().getId());
+        threadLocalDriver.get().get("http://ya.ru/");
     }
 }
--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,13 +1,48 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import spring.constructors.DriverData;
 
 @ContextConfiguration(locations={"/spring-config.xml"})
 public class TestRunner extends AbstractTestNGSpringContextTests {
 
+    protected ThreadLocal<WebDriver> threadLocalDriver = new ThreadLocal<>();
+
     @Autowired
     @Qualifier("driver")
-    protected DriverData driverData;
+    public DriverData driverData;
+
+    @BeforeMethod
+    public void init() {
+        threadLocalDriver.set(initWebDriver(driverData.getBrowser()));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void webDriverTearDown() {
+        threadLocalDriver.get().quit();
+    }
+
+    private WebDriver initWebDriver(String browser) {
+        WebDriver driver;
+        switch (browser) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+//            case "phantomjs":
+//                driver = new PhantomJSDriver();
+//                break;
+            default:
+                throw new RuntimeException("Unsopported broser [" + browser + "] exception");
+        }
+        return driver;
+    }
 }
